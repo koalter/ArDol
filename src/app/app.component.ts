9645cbecb3059c10bc4a0f3cfe6ec91b7c61c48e
koalter <heavens_drive090@hotmail.com>
2020-11-05 21:31:07 -0300

    primeras pruebas

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -12,15 +12,30 @@ export class AppComponent {
   title = 'ArDol';
   exchangeRatesObservable: Observable<any>;
   exchangeRates: string[];
+  width: number;
+  height: number;
+  isMobile: boolean;
+  mobileModo: boolean;
 
   constructor(public appService: AppService) {}
 
   ngOnInit() {
+    this.width = window.innerWidth;
+    this.height = window.innerHeight;
+    this.isMobile = this.width <= 360;
+    console.log(this.isMobile);
+    console.log(this.mobileModo);
+    
     this.exchangeRatesObservable = this.appService.getRates();
 
     this.exchangeRatesObservable.subscribe(rates => {
       this.exchangeRates = rates.currencies_alternatives;
     });
+  }
+
+  cambiarModo(modo: boolean): void {
+    this.mobileModo = modo;
+    console.log(this.mobileModo);
   }
 
 }
