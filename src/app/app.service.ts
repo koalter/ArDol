a25d32d53cf0577c8a61e95b4c274196f60a9ba6
koalter <heavens_drive090@hotmail.com>
2021-09-08 05:09:23 -0300

    AC 1-4

--- src/app/app.service.ts
+++ src/app/app.service.ts
@@ -1,6 +1,7 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Observable, of } from 'rxjs';
+import { IImpuesto } from './models/Interfaces';
 
 @Injectable({
   providedIn: 'root'
@@ -11,11 +12,27 @@ export class AppService {
 
   constructor(public http: HttpClient) {}
 
+  // TODO mover a un servicio dedicado (DolarService)
   public getRates(): Observable<any> {
     return this.http.get('https://www.currency-calc.com/currencies_rates.json');
   }
 
   public setWindowSize(width: number, height: number = null) {
     this.mobileView = width <= 450;
+  }
+
+  // TODO mover a un servicio dedicado (SteamService)
+  public getMontoSteam(montoInicial: number, impuestos: IImpuesto[]): number {
+    let montoFinal = montoInicial;
+    
+    for (let impuesto of impuestos) {
+      montoFinal += montoInicial * impuesto.valor;
+    }
+
+    return montoFinal;
+  }
+
+  public calcularImpuesto(base: number, impuesto: IImpuesto) {
+    return base * impuesto.valor;
   }
 }
--- src/app/conversor-steam/conversor-steam.component.ts
+++ src/app/conversor-steam/conversor-steam.component.ts
@@ -1,15 +1,52 @@
 import { Component, OnInit } from '@angular/core';
+import { AppService } from '../app.service';
+import { ConversorSteam } from '../models/ConversorSteam';
+import { IImpuesto } from '../models/Interfaces';
 
 @Component({
-  selector: 'app-conversor-steam',
+  selector: 'conversor-steam',
   templateUrl: './conversor-steam.component.html',
   styleUrls: ['./conversor-steam.component.css']
 })
 export class ConversorSteamComponent implements OnInit {
 
-  constructor() { }
+  header: string = 'Calculadora para Steam';
+  conversor: ConversorSteam;
+  currency: string;
+  amba: boolean = true;
+  impuestos: IImpuesto[];
+  
+  get tooltip(): string {
+    let texto = `Monto Inicial + 8% Impuesto PAIS + 21% IVA + 35% Ganancias`;
 
-  ngOnInit(): void {
+    if (this.amba) {
+      texto += ` + 2% IIBB (AMBA)`
+    }
+
+    return texto;
   }
 
+  constructor(public appService: AppService) { }
+
+  ngOnInit(): void {
+    this.conversor = new ConversorSteam();
+    this.currency = 'AR';
+    this.impuestos = this.conversor.impuestos;
+  }
+
+  filtrarImpuestos(): void {
+    this.impuestos = !this.amba ? 
+      this.conversor.impuestos 
+      : this.conversor.impuestos.filter(impuesto => !impuesto.nombre.includes('AMBA'));
+  }
+
+  calcularMonto(): string {
+    return this.conversor.valor ? 
+      this.appService.getMontoSteam(this.conversor.valor, this.impuestos).toFixed(2) 
+      : '';
+  }
+
+  calcularImpuesto(impuesto: IImpuesto): string {
+    return this.appService.calcularImpuesto(this.conversor.valor, impuesto).toFixed(2);
+  }
 }
